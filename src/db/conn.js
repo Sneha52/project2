c9455f6e359a8d3f5d4e371151882f052b64f8ac
Sneha52 <69627881+Sneha52@users.noreply.github.com>
2021-04-24 17:05:00 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/signup",{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/signup",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
